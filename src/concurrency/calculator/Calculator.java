a7af710e9ae05bfb501b13418f6d1620c8435e58
codeFunny71 <whynotlaugh@gmail.com>
2020-01-10 18:45:57 -0800

    worked on Calculator class and created threads for testing using main(psvm)

--- src/concurrency/calculator/Calculator.java
+++ src/concurrency/calculator/Calculator.java
@@ -1,4 +1,31 @@
 package concurrency.calculator;
 
-public class Calculator {
+public class Calculator implements Runnable{
+
+    @Override
+    public void run() {
+            long current = 1L;
+            long max = 20000L;
+            long numPrimes = 0L;
+
+            System.out.printf("Thread '%s': START\n", Thread.currentThread().getName());
+            while(current <= max){
+                if (isPrime(current)){
+                        numPrimes++;
+                }
+                current++;
+            }
+    }
+
+    private boolean isPrime(long number) {
+        if(number <= 2){
+            return false;
+        }
+        for (long i =2; i < number; i++){
+            if ((number%i)==0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
