91bf79644b7daccaf266434bc87bdbbbec79a016
codeFunny71 <whynotlaugh@gmail.com>
2020-03-27 13:54:26 -0700

    created a method to make change.

--- src/small_functions/main.java
+++ src/small_functions/main.java
@@ -33,8 +33,33 @@ public class main {
         return count;
     }
 
+    public static void makeChange(double changeDue) {
+        int change = (int)(Math.ceil(changeDue*100));
+        int dollars = Math.round((int)change/100);
+        change=change%100;
+        int quarters = Math.round((int)change/25);
+        change=change%25;
+        int dimes = Math.round((int)change/10);
+        change=change%10;
+        int nickels = Math.round((int)change/5);
+        change=change%5;
+        int pennies = Math.round((int)change/1);
+
+        System.out.println("Dollars: " + dollars);
+        System.out.println("Quarters: " + quarters);
+        System.out.println("Dimes: " + dimes);
+        System.out.println("Nickels: " + nickels);
+        System.out.println("Pennies: " + pennies);
+    }
+
 
     public static void main(String[] args) {
+
+        
+
+//        double change = 7.59;
+//        main.makeChange(change);
+
 
         //calculate area
 //        int radius = 5;
