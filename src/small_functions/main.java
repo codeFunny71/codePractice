266b22980de5aff58230d86fea976b8511d8bee5
codeFunny71 <whynotlaugh@gmail.com>
2020-03-27 12:33:30 -0700

    created method to compute the volume of a cylinder

--- src/small_functions/main.java
+++ src/small_functions/main.java
@@ -5,13 +5,29 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
 
+        //Compute the volume of a cylinder
+
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a degree in Celsius: ");
-        double celsius = input.nextDouble();
+        System.out.print("Enter the radius and the length of a cylinder: ");
+        double radius = input.nextDouble();
+        double length = input.nextDouble();
 
-        double fahrenheit = (9.0 / 5.0) * celsius + 32.0;
-        System.out.println(celsius + " degree Celsius is equal to " + fahrenheit + " in Fahrenheit");
+        double area = radius * radius * 3.14159265359;
+        double volume = area * length;
+
+        System.out.println("The area is " + area);
+        System.out.println("The volume is " + volume);
+
+        //Convert Celsius to Fahrenheit
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.print("Enter a degree in Celsius: ");
+//        double celsius = input.nextDouble();
+//
+//        double fahrenheit = (9.0 / 5.0) * celsius + 32.0;
+//        System.out.println(celsius + " degree Celsius is equal to " + fahrenheit + " in Fahrenheit");
 
 
 
